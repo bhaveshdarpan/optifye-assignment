b7b7e0dae92515cd00687ccd14fb2b50bb874b21
Bhavesh Darpan <tapsankavyes@gmail.com>
2026-02-09 11:31:18 +0530

    updating version for eks-- cost heavy suuport optimized

--- infrastructure/terraform/eks.tf
+++ infrastructure/terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 19.16"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.32"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
